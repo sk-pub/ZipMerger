2803683637d4103ec4925dd27f8e204b46edc120
Sergei Koshel <unicrus@users.noreply.github.com>
2022-04-15 00:56:30 +0200

    Initial commit
    Asynchronous download and zipping of files by their URLs

---
+++ ZipMerger/FileCallbackResult.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace ZipMerger
+{
+    /// <summary>
+    /// Represents an <see cref="ActionResult"/> that when executed will
+    /// execute a callback to write the file content out as a stream.
+    /// </summary>
+    public class FileCallbackResult : FileResult
+    {
+        private Func<Stream, ActionContext, Task> _callback;
+
+        /// <summary>
+        /// Creates a new <see cref="FileCallbackResult"/> instance.
+        /// </summary>
+        /// <param name="contentType">The Content-Type header of the response.</param>
+        /// <param name="callback">The stream with the file.</param>
+        public FileCallbackResult(string contentType, Func<Stream, ActionContext, Task> callback)
+            : base(contentType)
+        {
+            _callback = callback ?? throw new ArgumentNullException(nameof(callback));
+        }
+
+        /// <summary>
+        /// Gets the callback responsible for writing the file content to the output stream.
+        /// </summary>
+        public Func<Stream, ActionContext, Task> Callback => _callback;
+
+        /// <inheritdoc />
+        public override Task ExecuteResultAsync(ActionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var syncIOFeature = context.HttpContext.Features.Get<IHttpBodyControlFeature>();
+            if (syncIOFeature != null)
+            {
+                syncIOFeature.AllowSynchronousIO = true;
+            }
+
+            var executor = new FileCallbackResultExecutor(context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>());
+            return executor.ExecuteAsync(context, this);
+        }
+    }
+
+    /// <summary>
+    /// An action result handler of type file
+    /// </summary>
+    internal sealed class FileCallbackResultExecutor : FileResultExecutorBase
+    {
+        /// <summary>
+        /// Creating an instance of a class <see cref="FileCallbackResultExecutor"/>
+        /// </summary>
+        /// <param name="loggerFactory"></param>
+        public FileCallbackResultExecutor(ILoggerFactory loggerFactory)
+            : base(CreateLogger<FileCallbackResultExecutor>(loggerFactory))
+        { }
+
+        /// <summary>
+        /// Handler execution
+        /// </summary>
+        /// <param name="context">Action context</param>
+        /// <param name="result">Action result</param>
+        /// <returns><see cref="Task"/></returns>
+        public Task ExecuteAsync(ActionContext context, FileCallbackResult result)
+        {
+            SetHeadersAndLog(context, result, null, result.EnableRangeProcessing);
+            return result.Callback(context.HttpContext.Response.Body, context);
+        }
+    }
+}
---
+++ ZipMerger/MergeController.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Compression;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZipMerger
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MergeController : ControllerBase
+    {
+        // GET: api/<MergeController>
+        [HttpGet]
+        public FileResult Get([FromServices] HttpClient httpClient)
+        {
+            var file1 = (Name: "50MB.bin", Url: "https://sabnzbd.org/tests/internetspeed/50MB.bin", Compress: false);
+            var file2 = (Name: "100MB.bin", Url: "https://speed.hetzner.de/100MB.bin", Compress: false);
+            var file3 = (Name: "1GB.bin", Url: "https://speed.hetzner.de/1GB.bin", Compress: true);
+
+            var files = new [] { file1, file2, file3 };
+
+            // TODO: Stop downloading when the client gets disconnected
+            return new FileCallbackResult("application/zip", async (outputStream, _) =>
+            {
+                using (var zipArchive = new ZipArchive(outputStream, ZipArchiveMode.Create))
+                {
+                    var downloads = files.Select(async (file) =>
+                        (file.Name, file.Compress, Stream: await httpClient.GetStreamAsync(file.Url))
+                    ).ToList();
+
+                    while (downloads.Any())
+                    {
+                        var finishedDownload = await Task.WhenAny(downloads);
+                        downloads.Remove(finishedDownload);
+
+                        var file = await finishedDownload;
+
+                        var zipEntry = zipArchive.CreateEntry(
+                            file.Name,
+                            file.Compress ? CompressionLevel.Fastest : CompressionLevel.NoCompression
+                        );
+                        using (var sourceStream = file.Stream)
+                        using (var zipStream = zipEntry.Open())
+                        {
+                            await sourceStream.CopyToAsync(zipStream);
+                        }
+                    }
+                }
+            })
+            {
+                FileDownloadName = "merged.zip"
+            };
+        }
+    }
+}
