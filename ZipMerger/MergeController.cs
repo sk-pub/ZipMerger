7adc178d9083538c9aa38b5106b523e48afd2c70
Sergei Koshel <2191338+sk-pub@users.noreply.github.com>
2022-10-16 15:14:08 +0200

    Support unzipping on the fly to repack to the output stream

--- ZipMerger/MergeController.cs
+++ ZipMerger/MergeController.cs
@@ -1,5 +1,7 @@
-﻿using System.IO.Compression;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperLinq.Async;
+using System.IO.Compression;
+using TeilOne.FastZip;
 
 namespace ZipMerger
 {
@@ -11,43 +13,66 @@ namespace ZipMerger
         [HttpGet]
         public FileResult Get([FromServices] HttpClient httpClient)
         {
-            var file1 = (Name: "50MB.bin", Url: "https://sabnzbd.org/tests/internetspeed/50MB.bin", Compress: false);
-            var file2 = (Name: "100MB.bin", Url: "https://speed.hetzner.de/100MB.bin", Compress: false);
-            var file3 = (Name: "1GB.bin", Url: "https://speed.hetzner.de/1GB.bin", Compress: true);
+            var file1 = (Name: "50MB.bin", Url: "https://sabnzbd.org/tests/internetspeed/50MB.bin", Compress: false, Unzip: false);
+            var file2 = (Name: "100MB.bin", Url: "https://speed.hetzner.de/100MB.bin", Compress: false, Unzip: false);
+            var file3 = (Name: "1GB.bin", Url: "https://speed.hetzner.de/1GB.bin", Compress: true, Unzip: false);
 
-            var files = new [] { file1, file2, file3 };
+            var zipFile1 = (Name: "should-unzip", Url: "https://teil-one.s3.eu-central-1.amazonaws.com/zip-mixed.zip", Compress: true, Unzip: true);
+
+            var files = new[] { file1, file2, file3, zipFile1 };
 
             // TODO: Stop downloading when the client gets disconnected
             return new FileCallbackResult("application/zip", async (outputStream, _) =>
             {
-                using (var zipArchive = new ZipArchive(outputStream, ZipArchiveMode.Create))
+                using var zipArchive = new ZipArchive(outputStream, ZipArchiveMode.Create);
+
+                var downloads = files.Select(file => DownloadFiles(httpClient, file)).ConcurrentMerge();
+
+                await foreach (var file in downloads)
                 {
-                    var downloads = files.Select(async (file) =>
-                        (file.Name, file.Compress, Stream: await httpClient.GetStreamAsync(file.Url))
-                    ).ToList();
-
-                    while (downloads.Any())
+                    await using (var objectToDispose = file.ObjectToDispose)
                     {
-                        var finishedDownload = await Task.WhenAny(downloads);
-                        downloads.Remove(finishedDownload);
-
-                        var file = await finishedDownload;
-
                         var zipEntry = zipArchive.CreateEntry(
                             file.Name,
                             file.Compress ? CompressionLevel.Fastest : CompressionLevel.NoCompression
                         );
+
                         using (var sourceStream = file.Stream)
                         using (var zipStream = zipEntry.Open())
                         {
                             await sourceStream.CopyToAsync(zipStream);
                         }
                     }
+
+                    //await outputStream.FlushAsync();
                 }
+
+                GC.Collect();
             })
             {
                 FileDownloadName = "merged.zip"
             };
+        }
+
+        private static async IAsyncEnumerable<(string Name, bool Compress, Stream Stream, IAsyncDisposable ObjectToDispose)> DownloadFiles(HttpClient httpClient, (string Name, string Url, bool Compress, bool Unzip) file)
+        {
+            var url = new Uri(file.Url);
+
+            var fileStream = url.IsFile ? new FileStream(file.Url, FileMode.Open, FileAccess.Read) : await httpClient.GetStreamAsync(url);
+
+            if (file.Unzip)
+            {
+                await using var zipStreamReader = new ZipStreamReader(fileStream);
+
+                await foreach (var entry in zipStreamReader.GetEntriesAsync())
+                {
+                    yield return (Name: entry.FullName, file.Compress, Stream: entry.Stream, entry);
+                }
+            }
+            else
+            {
+                yield return (file.Name, file.Compress, Stream: fileStream, fileStream);
+            }
         }
     }
 }
